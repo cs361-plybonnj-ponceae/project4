7f6b3ad93707f188a0b379321ab1a11f57d0ffc4
nplybon <plybonnj@dukes.jmu.edu>
2022-03-22 15:31:43 -0400

    classifying

--- project4.c
+++ project4.c
@@ -44,29 +44,23 @@ int main(int argc, char *argv[])
                        // needs to classify
     int clusters_to_process; // This variable must contain the number of
                              // clusters a child process needs to classify
-
     // The user must supply a data file to be used as input
     if (argc != 2) {
         printf("Usage: %s data_file\n", argv[0]);
         return 1;
     }
-
     // Open input file for reading, exiting with error if open() fails
     input_fd = open(argv[1], O_RDONLY);
     if (input_fd < 0) {
         printf("Error opening file \"%s\" for reading: %s\n", argv[1], strerror(errno));
         return 1;
     }
-
     //use a struct to get size of input file
     struct stat sb;
-
     if (fstat(input_fd, &sb) == -1) {
         printf("Error: %s", strerror(errno));
         return 1;
     }
-
-
     // Open classification file for writing. Create file if it does not
     // exist. Exit with error if open() fails.
     classification_fd = open(CLASSIFICATION_FILE, O_WRONLY | O_CREAT, 0600);
@@ -74,33 +68,33 @@ int main(int argc, char *argv[])
         printf("Error creating file \"%s\": %s\n", CLASSIFICATION_FILE, strerror(errno));
         return 1;
     }
-
     // Create the pipe here. Exit with error if this fails.
     if (pipe(pipefd) < 0) {
         printf("Error: %s", strerror(errno));
-        exit(1);
+        return 1;
     }
-
     // The pipe must be created at this point
 #ifdef GRADING // do not delete this or you will lose points
     test_pipefd(pipefd, 0);
 #endif
-
     // total number of clusters in the input file
     int total_clusters = sb.st_size / CLUSTER_SIZE;
-
     // a counter used to keep track of cluster sizes that are not divisible by NUM_PROCESSES
     int remainder = total_clusters % NUM_PROCESSES + 1;
     // a variable used to remember the value of the remainder
     int r = remainder - 1;
     start_cluster = 0;
     clusters_to_process = total_clusters / NUM_PROCESSES;
-
     // REMOVE IN FINAL SUBMISSION
-    printf("Total file size: %lu bytes\n", sb.st_size);
-    printf("Total clusters: %d\n", total_clusters);
-   
-
+    //printf("Total file size: %lu bytes\n", sb.st_size);
+    //printf("Total clusters: %d\n", total_clusters);
+    //input file = 6207 c
+    //children = 5
+    //child1 1242 cluster 0
+    //child2 1242 cluster 1242
+    //child3 1241 cluster 2484
+    //child4 1241 cluster 3725
+    //child5 1241 cluster 4966
     // Fork NUM_PROCESS number of child processes
     for (int i = 0; i < NUM_PROCESSES; i++) {
         // decrement remainder on each iteration
@@ -115,43 +109,115 @@ int main(int argc, char *argv[])
             // for the child processes to be aware of which clusters
             // they need to process, classify them, and create a message
             // for each cluster to be written to the pipe.
-       
-        
-        printf("Child %d\n", i + 1);
         // with no remainder remaining
         if (remainder <= 0) {
-            // reset remainder to 0
-            remainder = 0;
             // compute the starting cluster
             start_cluster = clusters_to_process * i + r;
         } else {
             // tack a one onto every cluster that has the remainder still remaining
             clusters_to_process += 1;
             start_cluster = clusters_to_process * i;
+        } 
+        // REMOVE IN FINAL SUBMISSION
+        //printf("Child %d\n", i + 1);
+
+        //printf("\tStart cluster: %d\n", start_cluster);
+        //printf("\tClusters to process: %d\n", clusters_to_process);       
+        char cluster_data[CLUSTER_SIZE];
+        int bytes_read;
+        message.msg_cluster_number = start_cluster;
+
+
+        while ((bytes_read = read(input_fd, &cluster_data, CLUSTER_SIZE)) > 0) {
+        assert(bytes_read == CLUSTER_SIZE);
+        message.msg_cluster_type = TYPE_UNCLASSIFIED;
+
+
+        // Checks that the current cluster is of type JPG by looking for
+        // its body, and if it has either a header or a footer on each
+        // iteration. This helps avoid false negatives
+        if (has_jpg_body(cluster_data)) {
+            message.msg_cluster_type = TYPE_IS_JPG;
+            if (has_jpg_header(cluster_data)
+                    | has_jpg_footer(cluster_data)) {
+                message.msg_cluster_type = TYPE_IS_JPG;
+            }
         }
 
-        // REMOVE IN FINAL SUBMISSION
-        printf("\tStart cluster: %d\n", start_cluster);
-        printf("\tClusters to process: %d\n", clusters_to_process);
-            //input file = 6207 c
-            //children = 5
-            //child1 1242 cluster 0
-            //child2 1242 cluster 1242
-            //child3 1241 cluster 2484
-            //child4 1241 cluster 3725
-            //child5 1241 cluster 4966
-    
+        // Checks that the current cluster is of type HTML by looking for
+        // its body, and if it has either a header or a footer on each
+        // iteration. This helps avoid false negatives
+        if (has_html_body(cluster_data)) {
+            message.msg_cluster_type = TYPE_IS_HTML;
+            if (has_html_header(cluster_data)
+                    | has_html_footer(cluster_data)) {
+                message.msg_cluster_type = TYPE_IS_HTML;
+            }
+        }
+
+        // Check if the current cluster is a JPG header
+        // and set the file attribute to 0x03
+        if (has_jpg_header(cluster_data)) {
+            message.msg_cluster_type = TYPE_IS_JPG | TYPE_JPG_HEADER;
+        }
+
+        // Check if the current cluster is a HTML header
+        // and set the file attribute to 0x18
+        if (has_html_header(cluster_data)) {
+            message.msg_cluster_type = TYPE_IS_HTML | TYPE_HTML_HEADER;
+        }
+
+        // Check if the current cluster is a JPG footer
+        // and set the file attribute to 0x05
+        if (has_jpg_footer(cluster_data)) {
+            message.msg_cluster_type = TYPE_IS_JPG | TYPE_JPG_FOOTER;
+        }
+
+        // Check if the current cluster is a HTML footer
+        // and set the file attribute to 0x28
+        if (has_html_footer(cluster_data)) {
+            message.msg_cluster_type = TYPE_IS_HTML | TYPE_HTML_FOOTER;
+        }
+
+        // Single HTML contains both a header and a footer followed by zero bytes.
+        // Set the file attribtue to 0x38
+        if (has_html_header(cluster_data)
+                && has_html_footer(cluster_data)) {
+            message.msg_cluster_type = TYPE_IS_HTML | TYPE_HTML_HEADER | TYPE_HTML_FOOTER;
+        }
+
+        // Single JPG contains both a header and a footer followed by zero bytes.
+        // Set the file attribute to 0x07
+        if (has_jpg_header(cluster_data)
+                && has_jpg_footer(cluster_data)) {
+            message.msg_cluster_type = TYPE_IS_JPG | TYPE_JPG_HEADER | TYPE_JPG_FOOTER;
+        }
+
+        // Check if the current cluster is invalid
+        // and set the file attribute to 0x80
+        if (message.msg_cluster_type == TYPE_UNCLASSIFIED) {
+            message.msg_cluster_type = TYPE_UNKNOWN;
+        }
+
+
+        printf("Cluster Number %d\tType:%x\n",message.msg_cluster_number, message.msg_cluster_type);
+
+        write(pipefd[1], &message, 1);
+        message.msg_cluster_number++;
+        }
+
+
+     
             // At this point, the child must know its start cluster and
             // the number of clusters to process.
 #ifdef GRADING // do not delete this or you will lose points
             printf("Child process %d\n\tStart cluster: %d\n\tClusters to process: %d\n",
               getpid(), start_cluster, clusters_to_process);
 #endif
-
-
             // At this point the pipe must be fully set up for the child
             // This code must be executed before you start iterating over the input
             // file and before you generate and write messages.
+
 #ifdef GRADING // do not delete this or you will lose points
             test_pipefd(pipefd, getpid());
 #endif
